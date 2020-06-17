b94556abfab0f82d901ec9969c1b5cb7c89df7cc
Fqstalph <66720924+Fqstalph@users.noreply.github.com>
2020-06-18 00:30:43 +0200

    Update Main.php

--- src/Fqstalph/ItemsHub/Main.php
+++ src/Fqstalph/ItemsHub/Main.php
@@ -46,8 +46,7 @@ class Main extends PluginBase implements Listener
     public function onJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
         $this->onItem($player);
-        $message = str_replace("{player}", $player->getDisplayName());
-		$this->getConfig()->get("JoinMessage"));
+        $message = str_replace("{player}", $player->getDisplayName(), $this->getConfig()->get("JoinMessage"));
         $event->setJoinMessage($message);
     }
 	
@@ -58,8 +57,7 @@ class Main extends PluginBase implements Listener
 	
     public function onQuit(PlayerQuitEvent $event) {
 		$player = $event->getPlayer();
-        $message = str_replace("{player}", $player->getDisplayName();
-		$this->getConfig()->get("QuitMessage"));
+        $message = str_replace("{player}", $player->getDisplayName(), $this->getConfig()->get("QuitMessage"));
         $event->setQuitMessage($message);
 	}
 
